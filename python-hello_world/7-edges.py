79758693e6add84a7fcf4bafdbad75ab99a187cf
grace2560 <g.nalule@alustudent.com>
2023-02-09 09:58:53 -0800

    commit 4

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letter: aa
+   hhhhh
---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is object-oriented programming language with very clear syntax"
+str = str[10:37] + str[-23:-17] + str[:6]
+print(str)
