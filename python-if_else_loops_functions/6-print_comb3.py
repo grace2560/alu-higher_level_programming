4f1094cf20c8414538ace2bd9dc308f58de92e0d
grace2560 <g.nalule@alustudent.com>
2023-02-09 23:29:24 -0800

    m

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range (0, 89):
-    if (i //10) < (i % 10):
-        print("{:02d}" .formart(i), end=", ")
+    if(i // 10) < (i % 10):
+        print("{:02d}". format(i), end="")
 print(89)
