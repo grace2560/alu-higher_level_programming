9158eb26b24ebc0549c0fa5d97566740f51148c9
grace2560 <g.nalule@alustudent.com>
2023-02-09 22:59:37 -0800

    cmt

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -14,4 +14,4 @@ elif number < 0:
     if (abs(number) % 10) == 0:
         print("0 and is 0")
     else:
-        print(f" - {abs(number) % 10} and is less than 6 and not 0")
+        print(f" -{abs(number) % 10} and is less than 6 and not 0")
